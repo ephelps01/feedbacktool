128d44c34662f68ab11295b0bff44502c89379ac
ethan.phelps1 <ethan.phelps@ecdltd.co.uk>
2019-03-05 08:45:03 +0000

    Fixing grammar

--- src/config.php
+++ src/config.php
@@ -6,16 +6,16 @@ namespace Ecd\Feedbacktool;
 
 // General Config
 // User Identifier, What field you would like to identify the user that created the issue. This must be defined in your users table. This may be the 'name', 'username', 'first name' ect... collumn in your users table.
-$userIdentifier = 'name';
+$userIdentifier = '';
 
 // GitLab Credentials
 // Access Token Key
-$accessToken = 'AzUYwLnFgx2J4W76sBzR';
+$accessToken = '';
 
 // API URL
-$apiUrl = 'https://dev.ecdltd.com/api/v4/projects/';
+$apiUrl = '';
 
 // Project ID
-$projectId = 20;
+$projectId = ;
 
 ?>
